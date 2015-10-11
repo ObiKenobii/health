f9f8f0ab0227dff3e145889f874d49acb464b82c
ikarus92 <ikarus92@gmx.de>
2015-10-11 18:15:20 +0200

    -

--- src/health/DataBean.java
+++ src/health/DataBean.java
@@ -1,13 +1,21 @@
 package health;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class DataBean {
 	
 	private ArrayList<WellnessDatasetBean> data = new ArrayList<WellnessDatasetBean>();
+	
 	private short wellness;
+	private String weather;
+	private short temperature;
+	private short humidity;
+	private ArrayList<String> pollen;
+	private short heartrate;
 	private String comment;
 	
+		
 	public WellnessDatasetBean getDataset(int i) {
 		return this.data.get(i);
 	}
@@ -16,10 +24,17 @@ public class DataBean {
 		this.data.remove(i);
 	}
 	
+	//TODO: Input Validation!
 	public boolean addDataset() {
 		WellnessDatasetBean ds = new WellnessDatasetBean();
-		ds.setComment(this.comment);
+		ds.setDate(new Date());
 		ds.setWellness(this.wellness);
+		ds.setWeather(this.weather);
+		ds.setTemperature(this.temperature);
+		ds.setHumidity(this.humidity);
+		ds.setPollen(this.pollen);
+		ds.setHeartrate(this.heartrate);
+		ds.setComment(this.comment);
 		this.data.add(ds);
 		return true;
 	}
@@ -45,6 +60,46 @@ public class DataBean {
 
 	public void setComment(String comment) {
 		this.comment = comment;
+	}
+
+	public String getWeather() {
+		return weather;
+	}
+
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+
+	public short getHeartrate() {
+		return heartrate;
+	}
+
+	public void setHeartrate(short heartrate) {
+		this.heartrate = heartrate;
+	}
+
+	public short getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(short temperature) {
+		this.temperature = temperature;
+	}
+
+	public short getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(short humidity) {
+		this.humidity = humidity;
+	}
+	
+	public ArrayList<String> getPollen() {
+		return pollen;
+	}
+
+	public void setPollen(ArrayList<String> pollen) {
+		this.pollen = pollen;
 	}
 	
 }
