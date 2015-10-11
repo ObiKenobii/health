ddb38c6318c816f583f14fca343f4dcf7e1e2c2c
ikarus92 <ikarus92@gmx.de>
2015-10-11 12:28:27 +0200

    second commit.

--- src/health/DataBean.java
+++ src/health/DataBean.java
@@ -1,21 +1,8 @@
 package health;
 
 public class DataBean {
-	private String name;
 	private short wellness;
 	private String comment;
-
-    public String getName ()
-    {
-        return name;
-    }
-
-
-    public void setName (final String name)
-    {
-        this.name = name;
-    }
-
 
 	public short getWellness() {
 		return wellness;
